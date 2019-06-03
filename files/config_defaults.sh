8649fdba5ca8fb9c84805108eb0c6cbad3c57170
Martin Hübner <martin.hubner@web.de>
2019-06-03 13:55:56 +0200

    set date of the last upgrade to date of first bootup

--- files/config_defaults.sh
+++ files/config_defaults.sh
@@ -5,4 +5,10 @@ HOUR=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
 MIN=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
 echo "$MIN $HOUR * * 2	/usr/bin/autoupdate a" >> /etc/crontabs/root
 /etc/init.d/cron restart
+
+# set LAST_UPGR to the date of the first reboot
+TODAY=$(date -u +%Y%m%d)
+uci set autoupdate.upgrade_date.last_upgr="$TODAY"
+uci commit autoupdate
+
 exit 0
