4eaad511b7643f30d631fe3636d79c59a0ee1fdb
Martin Hübner <martin.hubner@web.de>
2019-06-10 12:48:44 +0200

    * set LAST_UPGR only, if system has internet connection (thus ntp acces) * avoid more than one job for autoupdate in crontab

--- files/config_defaults.sh
+++ files/config_defaults.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 #
-# get a fairly random update-time, to protect the servers from DoS
-HOUR=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
-MIN=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
-echo "$MIN $HOUR * * 2	/usr/bin/autoupdate a" >> /etc/crontabs/root
-/etc/init.d/cron restart
 
-# set LAST_UPGR to the date of the first reboot
-TODAY=$(date -u +%Y%m%d)
-uci set autoupdate.upgrade_date.last_upgr="$TODAY"
-uci commit autoupdate
+#if autoupdate is not present in crontab, include it.
+crontab -l | grep /usr/bin/autoupdate >> /dev/null
+if [ $? != 0 ]; then
+  #get a fairly random update-time, to protect the servers from DoS
+  HOUR=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 24))
+  MIN=$(( $(tr -cd 0-9 </dev/urandom | head -c 2) % 60))
+  echo "$MIN $HOUR * * 4        /usr/bin/autoupdate a" >> /etc/crontabs/root
+  /etc/init.d/cron restart
+fi
 
-exit 0
+#set LAST_UPGR to the date of the first reboot. Ensure, that the router has
+#internet connection (thus ntp access)
+ping -c3 8.8.8.8
+if [ $? = 0 ]; then
+  TODAY=$(date -u +%Y%m%d)
+  uci set autoupdate.upgrade_date.last_upgr="$TODAY"
+  uci commit autoupdate
+  exit 0
+else
+  exit 1
+fi
